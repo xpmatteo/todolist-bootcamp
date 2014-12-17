6f277925da73beca8a733e0b8264b9d7ed47f91a
nitrol88 <giankidipaol@gmail.com>
2014-12-17 15:48:39 +0100

    Fixed POST parameters

--- src/main/java/it/xpug/helloworld/HelloWorldServlet.java
+++ src/main/java/it/xpug/helloworld/HelloWorldServlet.java
@@ -2,14 +2,11 @@ package it.xpug.helloworld;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io.IOUtils;
 
 public class HelloWorldServlet extends HttpServlet {
 
@@ -18,19 +15,15 @@ public class HelloWorldServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		getToDoList(request, response);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		addToDo(request, response);
 	}
 
-	private void getToDoList(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
+	private void getToDoList(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		ToDoListJsonSerializer serializer = new ToDoListJsonSerializer(toDoList);
 
 		response.setContentType("application/json");
@@ -40,14 +33,7 @@ public class HelloWorldServlet extends HttpServlet {
 	}
 
 	private void addToDo(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		StringWriter writer = new StringWriter();
-
-		try {
-			IOUtils.copy(request.getReader(), writer);
-		}
-		catch (IOException e) {}
-
-		String json = String.format("{\"text\":\"%s\"}", writer.toString());
+		String json = String.format("{\"text\":\"%s\"}", request.getParameter("toDoText"));
 		toDoList.addToDo(new ToDoJsonSerializer(json).deserialize());
 
 		getToDoList(request, response);
